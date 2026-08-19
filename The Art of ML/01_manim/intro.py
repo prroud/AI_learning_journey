13e7e816431dc30875ebb370355f55697d1febbf
Eryk Orzech <erykorzech05@gmail.com>
2026-08-19 21:27:46 +0200

    first work on intro

--- The Art of ML/01_manim/intro.py
+++ The Art of ML/01_manim/intro.py
@@ -1,9 +1,27 @@
 from manim import *
+import os
 
-class Pith(Scene):
+class Intro(Scene):
     def construct(self):
-        name = Tex("The Art of Machine Learning")
+        self.camera.background_color = "#020e21"
+
+        # Scene 1 - channel name
+
+        channel_name = Text("The Art of Machine Learning", font_size=52, weight=BOLD)
+        channel_name[:10].set_color("#94A3B8")
+        channel_name[:10].set_color("#38BDF8")
+
+        underline = Line(LEFT, RIGHT, color="#38BDF8", stroke_width=3)
+        underline.match_width(channel_name)
+        underline.next_to(channel_name, DOWN, buff=0.25)
+
+        intro_group = VGroup(channel_name, underline).move_to(ORIGIN)
+
+        self.play(Write(channel_name), run_time=1.8)
+        self.play(Create(underline), run_time=0.6)
+        self.wait(2.5)
+
+        self.play(FadeOut(intro_group, shift=UP * 0.4), run_time=0.8)
+        self.wait(0.3)
 
 
-        self.play(Write(name))
-        self.wait()
